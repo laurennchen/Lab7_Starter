3de6cff33314cbaf887814550a0ffa4ecb2a1f6c
laurennchen <lmchen@ucsd.edu>
2022-11-08 22:52:37 -0800

    part 1 expose done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -68,6 +68,10 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  let recipes = localStorage.getItem('recipes');
+  if (recipes != null) {
+    return JSON.parse(recipes);
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
@@ -77,9 +81,28 @@ async function getRecipes() {
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+  var recipesArray = [];
+  return new Promise(newFunction);
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
+  async function newFunction(resolve, reject) {
+    for (let i = 0; i < RECIPE_URLS.length; i++) {
+      try {
+        let url = await fetch(RECIPE_URLS[i]);
+        let url_json = await url.json();
+        recipesArray.push(url_json);
+
+        if (i == RECIPE_URLS.length-1) {
+          saveRecipesToStorage(recipesArray);
+          resolve(recipesArray);
+        }
+      } catch (e) {
+        console.error(e);
+        reject(e);
+      }
+    }
+  }
   /**************************/
   // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
   //            declared above
