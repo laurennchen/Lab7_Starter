30720532d25e40c664a7e910434682e5954e509b
laurennchen <lmchen@ucsd.edu>
2022-11-09 10:27:31 -0800

    pt 2 progress- getting WebSocket connection errors

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,6 +45,18 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', (event) => {
+      try {
+        const registration = navigator.serviceWorker.register("./sw.js", {
+          scope: "./",
+        });
+        console.log("Registration successful!");
+      } catch {
+        console.log("Registration failed.");
+      }
+    });
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register '/sw.js' as a service worker (The MDN article
